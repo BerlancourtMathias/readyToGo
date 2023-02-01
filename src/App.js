736370db6da7fecb586838e7ddc5d6390b8dc91e
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-02-01 15:01:09 +0100

    commit pour ma première issue de l'exercice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [switch1, setSwitch1] = useState(false);
+  const [switch2, setSwitch2] = useState(false);
+  const [switch3, setSwitch3] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header>⚛︎ Ready To Go</header>
+      <div className="container">
+        <div className="column1">
+          <div className="switch1">
+            <button
+              onClick={() => {
+                setSwitch1(true);
+              }}
+            >
+              ON
+            </button>
+            <button>OFF</button>
+          </div>
+          <div className="switch2"></div>
+          <div className="switch3"></div>
+        </div>
+        <div className="column2"> </div>
+      </div>
+      <footer>
+        Made with <span>&nbsp;React&nbsp;</span>
+        at <span>&nbsp;Le Reacteur&nbsp;</span>
+        by
+        <span>&nbsp;Mathias&nbsp;</span>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
