5f081451af0373ba7b469a0b9def2d6b00662b4c
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-02-01 16:57:00 +0100

    exercice terminé avec un design qui laisse à désiré, le code est correct

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   const [switch2, setSwitch2] = useState(false);
   const [switch3, setSwitch3] = useState(false);
   return (
-    <div>
+    <div className="body">
       <header>⚛︎ Ready To Go</header>
       <div className="container">
         <div className="column1">
@@ -15,15 +15,65 @@ const App = () => {
               onClick={() => {
                 setSwitch1(true);
               }}
+              className={switch1 ? "violetClass" : "whiteClass"}
             >
               ON
             </button>
-            <button>OFF</button>
+            <button
+              onClick={() => {
+                setSwitch1(false);
+              }}
+              className={switch1 ? "whiteClass" : "violetClass"}
+            >
+              OFF
+            </button>
           </div>
-          <div className="switch2"></div>
-          <div className="switch3"></div>
+          <div className="switch2">
+            <button
+              onClick={() => {
+                setSwitch2(true);
+              }}
+              className={switch2 ? "violetClass" : "whiteClass"}
+            >
+              ON
+            </button>
+            <button
+              onClick={() => {
+                setSwitch2(false);
+              }}
+              className={switch2 ? "whiteClass" : "violetClass"}
+            >
+              OFF
+            </button>
+          </div>
+          <div className="switch3">
+            <button
+              onClick={() => {
+                setSwitch3(true);
+              }}
+              className={switch3 ? "violetClass" : "whiteClass"}
+            >
+              ON
+            </button>
+            <button
+              onClick={() => {
+                setSwitch3(false);
+              }}
+              className={switch3 ? "whiteClass" : "violetClass"}
+            >
+              OFF
+            </button>
+          </div>
         </div>
-        <div className="column2"> </div>
+        <div className="column2">
+          {/* <div className="displayStart"> */}
+          {switch1 && switch2 && switch3 ? (
+            <div className="displayStartGo">GO ! </div>
+          ) : (
+            <div className="displayStartNoway">NO WAY ! </div>
+          )}
+          {/* </div> */}
+        </div>
       </div>
       <footer>
         Made with <span>&nbsp;React&nbsp;</span>
